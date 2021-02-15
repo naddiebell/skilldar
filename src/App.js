8318d34cad221ee537edf5466b25a16466c159b0
Natalie Whitton <newhitton@gmail.com>
2021-02-15 16:10:02 +0100

    add header image

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import NavBar from "./Components/NavBar/NavBar"
 
 function App() {
 
-  
+
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <header className="header">
+        <img src="https://res.cloudinary.com/savely/image/upload/v1613401190/cinema/oxygen-nature-header_qwzmxf.webp" />
+      </header>
+    </>
   );
 }
 
