9e393cd0ec977fd269e42a6df303400abfd8e0e9
Natalie Whitton <newhitton@gmail.com>
2021-02-15 15:40:30 +0100

    add basic nav bar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavBar.css"; 
+
+function NavBar() {
+
+    return (
+       
+        <ul className="navBar">
+            <li>
+                <a href="#" className="skilldarLogo"></a>
+            </li>
+        </ul>
+      
+    );
+}
+
+export default NavBar;
