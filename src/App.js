a357d4e86c588a10617b1355bb1ee132ef69fa1e
Natalie Whitton <newhitton@gmail.com>
2021-02-15 19:39:50 +0100

    made mock data with Mockaroo, then call it on App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import NavBar from "./Components/NavBar/NavBar"
+import Home from "./Pages/Home/Home"
 
 function App() {
+  const [data, setData] = useState();
 
+  useEffect(async () => {
+    const result = await axios(
+      'https://my.api.mockaroo.com/skilldar.json?key=3a6cc520',
+    );
+
+    setData(result.data);
+  }, [])
+
+  console.log("data", data)
 
   return (
     <>
       <NavBar />
-      <header className="header">
-        <img src="https://res.cloudinary.com/savely/image/upload/v1613401190/cinema/oxygen-nature-header_qwzmxf.webp" />
-      </header>
+      <Home />
     </>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+
+    return (
+        <>
+            <header className="header">
+                <div className="headerText">
+                    <h2 className="headerTitle">Creative motion projects</h2>
+                    <p className="headerSubtitle">Discover client work to pitch on.</p>
+                    {/* <h6>Video By Harold Miles</h6> */}
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Home;
